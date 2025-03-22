61ccc43660e513db192e7d721a06eeb1d9fc0b8f
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-03-22 21:12:42 +0530

    iiddleware bulitin middleware headers statuscode

---
+++ Day 70 - March - 22 - 2025/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Middle ware 1");
+  req.name = "sabed";
+  next();
+});
+
+// builtin middleware
+app.use(express.json());
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Middle ware 2", req.name);
+  next();
+});
+
+// headers
+app
+  .route("/api/data")
+  .get((req, res) => {
+    console.log(req.headers); // Logs request headers
+    console.log(res.getHeaders()); // Logs response headers (before sending response)
+    res.send("Hello world!");
+  })
+
+  // Statuscode
+  .post((req, res) => {
+    console.log(req.body);
+    return res.status(200).json({
+      massage: "OK",
+    });
+  });
+
+app.listen(PORT, () => {
+  console.log(`click http://localhost:${PORT}`);
+});
